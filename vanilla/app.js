51f69d089aebfff8966d48d369a4771543922b29
Ana Lombera <nuskylombera@gmail.com>
2025-09-05 13:12:33 +0200

    cambios en array de tareas

--- vanilla/app.js
+++ vanilla/app.js
@@ -1,7 +1,12 @@
-
 const tareas = ["tarea1", "tarea2", "tarea3"];
 
-for (let i = 0; i <= tareas.length; i++) {
-    console.log( tareas[i] );
+for (let i = 0; i < tareas.length; i++) {
+
+
+    const item = document.createElement('li')
+    item.innerText = tareas[i];
+
+    document.getElementById("listaTareas").appendChild(item);
+
 }
 
