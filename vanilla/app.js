c128b849924090de831652c19f911586ba79b450
AnaLombera <nuskylombera@gmail.com>
2025-09-09 12:28:30 +0200

    Feature/state management vanilla (#3)
    * Ejercicio parte 2
    
    * cambios en array de tareas
    
    * creacion de un formulario
    
    * cambio de boton a submit
    
    * creacion de localSorage
    
    * limpiar listado

---
+++ vanilla/app.js
@@ -0,0 +1,46 @@
+const tareas = JSON.parse(localStorage.getItem("tareas")) || [];
+
+const nuevatarea = document.getElementById("nuevaTarea");
+
+const lista = document.getElementById("listaTareas");
+
+const formulario = document.getElementById("add-new-task-form");
+
+
+function mostrarTareas() {
+    lista.innerHTML = "";
+    tareas.forEach((tarea) => {
+        let li = document.createElement("li");
+        li.textContent = tarea;
+        lista.appendChild(li);
+    });
+}
+
+formulario.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const inputvalido = nuevatarea.value.trim();
+
+
+    if (inputvalido !== "") {
+        tareas.push(inputvalido); // añadir al array
+        localStorage.setItem("tareas", JSON.stringify(tareas));
+        nuevatarea.value = "";
+        nuevatarea.focus();
+        mostrarTareas();
+
+    }
+
+
+});
+
+mostrarTareas();
+
+
+
+
+
+
+
+
+
+
