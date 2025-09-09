c5947a9713b0ff7f05f9d21a66bef2d03471549d
Ana Lombera <nuskylombera@gmail.com>
2025-09-09 10:56:22 +0200

    creacion de localSorage

--- vanilla/app.js
+++ vanilla/app.js
@@ -1,4 +1,4 @@
-const tareas = [];
+const tareas = JSON.parse(localStorage.getItem("tareas")) || [];
 
 const nuevatarea = document.getElementById("nuevaTarea");
 
@@ -7,21 +7,38 @@ const lista = document.getElementById("listaTareas");
 const formulario = document.getElementById("add-new-task-form");
 
 
+function mostrarTareas() {
+    tareas.forEach((tarea) => {
+        let li = document.createElement("li");
+        li.textContent = tarea;
+        lista.appendChild(li);
+    });
+}
+
 formulario.addEventListener("submit", (event) => {
     event.preventDefault();
     const inputvalido = nuevatarea.value.trim();
 
 
     if (inputvalido !== "") {
-        const li = document.createElement("li");
-        li.textContent = inputvalido;
-        lista.appendChild(li);
+        tareas.push(inputvalido); // añadir al array
+        localStorage.setItem("tareas", JSON.stringify(tareas));
         nuevatarea.value = "";
         nuevatarea.focus();
+        mostrarTareas();
 
     }
 
-})
+
+});
+
+mostrarTareas();
+
+
+
+
+
+
 
 
 
