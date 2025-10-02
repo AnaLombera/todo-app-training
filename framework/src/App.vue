5a377701d62104000e86cffd9f1130adad6c1aec
Ana Lombera <nuskylombera@gmail.com>
2025-10-02 10:23:24 +0200

    creacion de la barra para subir tareas

--- framework/src/App.vue
+++ framework/src/App.vue
@@ -1,15 +1,15 @@
 <script setup lang="ts">
-
-
-
-
+import BaseFooter from '@/components/BaseFooter.vue'
+import ToDoList from "@/components/ToDoList.vue";
 </script>
 
 <template>
   <h1>To Do App</h1>
-  <p>
-    Lista de teras
-  </p>
+  <p>Lista de teras</p>
+
+  <ToDoList/>
+
+  <BaseFooter />
 </template>
 
 <style scoped></style>
