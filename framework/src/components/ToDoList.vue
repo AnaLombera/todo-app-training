1f90d9e540d602338e053f1cd241ed67588a673a
AnaLombera <nuskylombera@gmail.com>
2025-10-02 10:18:02 +0200

    Creacion de componentes de listado (#7)

---
+++ framework/src/components/ToDoList.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+type ToDo = {
+  title: string;
+
+}
+
+const toDoList = ref<ToDo[]>([])
+
+
+
+const title = ref('')
+
+
+function onFormSubmit(event: SubmitEvent) {
+  event.preventDefault();
+
+  addToDo()
+}
+
+
+function addToDo() {
+  if (title.value.trim() !== "") {
+    const newTodo: ToDo = {
+      title: title.value,
+
+    };
+
+    toDoList.value.push(newTodo)
+
+    title.value = ""
+  }
+}
+
+
+
+
+
+</script>
+
+<template>
+  <div>
+    <form @submit="onFormSubmit">
+      <input placeholder="Escribir nueva tarea" v-model="title" type="text"/>
+      <button>Agregar Tarea</button>
+    </form>
+
+
+    <ul>
+      <li v-for="(toDo, index) in toDoList" :key="index">
+        {{ toDo.title }}
+      </li>
+    </ul>
+  </div>
+
+
+</template>
+
+<style scoped>
+
+</style>
