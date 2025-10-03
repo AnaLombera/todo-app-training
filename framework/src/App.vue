e53f9a80e9aec8c3af4cec1a82871aeb0a372d1b
AnaLombera <nuskylombera@gmail.com>
2025-10-03 10:44:00 +0200

    creacion de stores (#10)

--- framework/src/App.vue
+++ framework/src/App.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import BaseFooter from '@/components/BaseFooter.vue'
-import ToDoList from "@/components/ToDoList.vue";
-import ToDoForm from "@/components/ToDoForm.vue";
+import ToDoList from '@/components/ToDoList.vue'
+import ToDoForm from '@/components/ToDoForm.vue'
 </script>
 
 <template>
   <h1>To Do App</h1>
   <p>Lista de teras</p>
 
-  <ToDoForm/>
+  <ToDoForm />
 
-  <ToDoList/>
+  <ToDoList />
   <BaseFooter />
 </template>
 
--- framework/src/components/ToDoList.vue
+++ framework/src/components/ToDoList.vue
@@ -1,23 +1,17 @@
 <script setup lang="ts">
-import {useToDoState} from "@/composables/useToDoState.ts";
+import { useToDosStore } from '@/stores/todos.ts'
 
-const { toDoList } = useToDoState()
-
-
+const store = useToDosStore()
 </script>
 
 <template>
   <div>
     <ul>
-      <li v-for="(toDo, index) in toDoList" :key="index">
+      <li v-for="(toDo, index) in store.toDoList" :key="index">
         {{ toDo.title }}
       </li>
     </ul>
   </div>
-
-
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
