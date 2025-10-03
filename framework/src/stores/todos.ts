6adcc7b53d6d6c9a6537dd6f423498d5c3f41617
AnaLombera <nuskylombera@gmail.com>
2025-10-03 11:51:07 +0200

    conexion para sacar datos de la Api (#11)

--- framework/src/stores/todos.ts
+++ framework/src/stores/todos.ts
@@ -9,8 +9,22 @@ export const useToDosStore = defineStore('todos', () => {
     toDoList.value.push(newTodo)
   }
 
+  const loadToDos = async () => {
+    const apiURL = 'https://68bffd4a0b196b9ce1c2da70.mockapi.io/todos'
+    const response = await fetch(apiURL, {
+      headers: {
+        Accept: 'application/json',
+      },
+    })
+
+    toDoList.value = (await response.json()) as ToDo[]
+  }
+
   return {
+    // state
     toDoList: toDoList,
+    // actions
     addToDo,
+    loadToDos,
   }
 })
